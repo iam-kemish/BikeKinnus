5e8d6ba7a2e3f555ddf8213bba7264c0fcd8efd8
kemish bajgai <kemishbajgai4@gmail.com>
2024-11-27 16:34:55 +0545

    Add project files.

---
+++ Bookify/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext): base(dbContext)
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
---
+++ Bookify/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookify.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder {  get; set; }
+
+    }
+}
