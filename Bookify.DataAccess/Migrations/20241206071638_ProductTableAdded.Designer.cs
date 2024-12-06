b231bc536f19eb310f12cbe503228ecf1ea4385a
kemish bajgai <kemishbajgai4@gmail.com>
2024-12-06 13:05:25 +0545

    Product model updated to database

--- Bookify.DataAccess/Migrations/20241206071638_ProductTableAdded.Designer.cs
+++ Bookify.DataAccess/Migrations/20241206071638_ProductTableAdded.Designer.cs
@@ -3,6 +3,7 @@ using Bookify.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bookify.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241206071638_ProductTableAdded")]
+    partial class ProductTableAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
