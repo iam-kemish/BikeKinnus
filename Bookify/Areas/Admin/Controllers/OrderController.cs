0451fe43e9deccdaa838628fc96f1d6fab41063d
kemish bajgai <kemishbajgai4@gmail.com>
2025-01-06 12:50:29 +0545

    Created an Order Controller action method to manage orders.

---
+++ Bookify/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using BikeKinnus.DataAccess.Repositary;
+using BikeKinnus.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeKinnus.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeader _IOrderHeader;
+        private readonly IOrderDetail _IOrderDetail;
+
+        public OrderController(IOrderDetail orderDetail, IOrderHeader orderHeader)
+        {
+            _IOrderHeader = orderHeader;
+            _IOrderDetail = orderDetail;
+        }
+        public IActionResult Index()
+        {
+            List <OrderHeader> OrderHeaders = _IOrderHeader.GetAll(includeProperties: "AppUser").ToList();
+            return View(OrderHeaders);
+        }
+    }
+}
