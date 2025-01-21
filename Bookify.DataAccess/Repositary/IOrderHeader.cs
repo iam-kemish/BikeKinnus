baf930c48cd13b9b13ca0c467920fec078625526
kemish bajgai <kemishbajgai4@gmail.com>
2025-01-21 16:13:27 +0545

    Some changes made in orderHeader Repositary.

--- Bookify.DataAccess/Repositary/IOrderHeader.cs
+++ Bookify.DataAccess/Repositary/IOrderHeader.cs
@@ -6,6 +6,8 @@ namespace BikeKinnus.DataAccess.Repositary
     public interface IOrderHeader:IRepositary<OrderHeader>
     {
         void Update(OrderHeader orderHeader);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
         void Save();
     }
 }
